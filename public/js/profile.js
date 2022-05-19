ce00c23b324d1b09acc6730cabaa8b3dac100bba
Arshnoor03 <aboparai18@my.bcit.ca>
2022-05-19 22:51:23 +0530

    added doctor.html

--- public/js/profile.js
+++ public/js/profile.js
@@ -215,9 +215,9 @@ $(document).ready(function () {
                     s += `</div>`
                     s += `<div class="btn d-flex justify-content-center mt-3">`
                     s += `<a href="profile?id=${post._id}" class="btn btn-primary mx-2 br" id="EditCardButton">Edit post</a>`
-                   // s += `<button type="button" class="btn btn-danger mx-2 br" id="DeleteCardButton">Delete post</button>`
+                    s += `<button type="button" class="btn btn-danger mx-2 br" id="DeleteCardButton">Delete post</button>`
                     s += `<div class="btn d-flex justify-content-center mt-3" id="${post._id}">`
-                   // s += `<button type="button" class="btn btn-primary mx-2 br" id="EditCardButton">Edit post</button>`
+                    s += `<button type="button" class="btn btn-primary mx-2 br" id="EditCardButton">Edit post</button>`
                     s += `<button type="button" class="btn btn-secondary mx-2 br" id="DeleteCardButton">Delete post</button>`
                     s += `</div>`
                     s += `</div>`
